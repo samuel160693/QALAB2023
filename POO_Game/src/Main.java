6d6b73c560364b21ae0b4f6c26893db7ab1c54f2
Samuel Celis <samjcelis@gmail.com>
2024-01-26 23:10:12 -0500

    Versionamiento 2 feature-hu-292

--- POO_Game/src/Main.java
+++ POO_Game/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
+        //"Editado por Oskars Samuel Jhovanny Celis Arroyo "
 
         Toyota toyo = new Toyota("PAS201"); //200
         toyo.encenderMotor();
